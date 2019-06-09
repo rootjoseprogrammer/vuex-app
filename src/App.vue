1d5f06459c57a6c704343dd586fed293fef25553
Jose Diaz <root.jose.programmer@gmail.com>
2019-06-09 16:10:28 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <AddTodo />
+      <FilterTodo />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import FilterTodo from './components/FilterTodos'
+
+export default {
+  name: 'app',
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodo
+  }
+}
+</script>
+
+<style>
+img{
+  width: 90px;
+}
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
